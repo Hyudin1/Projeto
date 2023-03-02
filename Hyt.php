f11906a3cb1a4f87993a6b4fbee245c379f288bc
Hyudin1 <renan.tobaro@gmail.com>
2023-03-02 11:22:47 -0300

    versão1

---
+++ Hyt.php
@@ -0,0 +1,23 @@
+<?php // Início do código PHP
+
+//criação das váriaveis
+$var1 = 1;
+$var2 = 'texto';
+$var3 = true;
+$var4 = 2;
+$var5 = 3;
+$result = $var4 + $var5;
+
+
+Echo "O resultado de $var4 + $var5 = $result <br/>";
+echo 'Os valores das váraveis são: <br/>';
+echo 'var1 = $var1 <br/>';
+echo 'var2 = $var2 <br/>';
+echo "A soma de $var4 + $var5 = 5 <br/>";
+echo "A subtração de $var4 - $var5 = 1 <br/>";
+echo "A multiplicação de $var4 * $var5 = 6 <br/>";
+echo "A divisão de $var5 / $var4 = 1,5"
+
+// Fim do código PHP
+?>
+
