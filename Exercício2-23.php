ef276e824a19c0aea669ec0d8395b35b22e4b51c
Hyudin1 <126791431+Hyudin1@users.noreply.github.com>
2023-03-23 12:09:08 -0300

    Aula 23/03
    Operadores relacionais e lógicos part.II

---
+++ Exercício2-23.php
@@ -0,0 +1,22 @@
+<?php
+$V1 = 101;
+
+ if ($V1 <= 100) {
+    echo "Se ($V1) for menor que 100, o valor será 0";
+ }
+ if ($V1 > 100) {
+    echo "Se ($V1) for maior que 100, será impresso";
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ Exercício5-23.php
@@ -0,0 +1,19 @@
+<?php
+$n1 = 11;
+$n2 = 5;
+
+if($n1 > $n2){
+    Echo "Do maior ($n1) para o menor ($n2)";
+}
+else {
+    Echo "Do maior ($n2) para o menor ($n1)";
+}
+
+
+
+
+
+
+
+
+?>
