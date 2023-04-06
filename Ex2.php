8598a3229a9c3ba46fc7eb63a2b8fcefd6e293f1
Hyudin1 <126791431+Hyudin1@users.noreply.github.com>
2023-04-06 12:16:11 -0300

    Aula 06/04
    Aula sobre algoritimo While de repetição

---
+++ Ex2.php
@@ -0,0 +1,25 @@
+<?php
+
+// valores
+$inicial = 1;
+$final = 100;
+
+while ($final >= $inicial) {
+    echo "{$final} >= {$inicial} | {$inicial} <br/>";
+    $inicial++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
